20b6bebb4f32cb2dbbe30acaba2d04deb70dd8b8
cryptofedge <cryptofedge@gmail.com>
2026-08-14 17:52:08 -0400

    Shrink the sticky menu header and stop the wordmark colliding with the cart on phones

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *
  * Bump VERSION on every deploy that changes index.html.
  */
-const VERSION = "gp-v3";
+const VERSION = "gp-v4";
 const SHELL = ["./", "./index.html", "./assets/logo.png"];
 
 self.addEventListener("install", e => {
