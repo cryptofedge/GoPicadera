f3760dbb54a568c5ad8c70d242bba1b0a981ff5e
cryptofedge <cryptofedge@gmail.com>
2026-08-26 09:50:39 -0400

    Update site build

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *
  * Bump VERSION on every deploy that changes index.html.
  */
-const VERSION = "gp-v70";
+const VERSION = "gp-v72";
 const SHELL = ["./", "./index.html", "./assets/logo.png"];
 
 self.addEventListener("install", e => {
